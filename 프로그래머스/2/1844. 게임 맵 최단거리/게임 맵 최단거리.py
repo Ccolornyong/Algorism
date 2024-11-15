2cb09f9720f05f5579f96a9191d90e5db2a5c918
Ccolornyong <126884402+Ccolornyong@users.noreply.github.com>
2024-11-15 10:09:24 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.85 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,22 +1,22 @@
 from collections import deque
 
 def solution(maps):
-    n = len(maps) # 맵 끝의 x 좌표
-    m = len(maps[0]) # 맵 끝의 y 좌표
+    n = len(maps)
+    m = len(maps[0])
     
-    x, y = 0,0
-    queue = deque([(x, y)])
+    rx = [1,-1,0,0]
+    ry = [0,0,1,-1]
     
-    rx = [1, -1, 0, 0] # 좌,우,앞,뒤
-    ry = [0, 0, 1, -1] # 좌,우,앞,뒤
+    queue = deque([(0,0)])
     
     while queue:
-        x, y = queue.pop() 
+        x, y = queue.pop()
+        
         for i in range(4):
-            nx = x + rx[i]
-            ny = y + ry[i]
+            nx = rx[i] + x
+            ny = ry[i] + y
             if 0<=nx<n and 0<=ny<m and maps[nx][ny] == 1:
-                queue.appendleft((nx,ny))
+                queue.appendleft((nx, ny))
                 maps[nx][ny] = maps[x][y] + 1
-    
-    return maps[n-1][m-1] if maps[n-1][m-1] > 1 else -1
+                
+    return maps[n-1][m-1] if maps[n-1][m-1] != 1 else -1
