c151e12f05596ee71a152484a63e1e5003c278ea
Ccolornyong <126884402+Ccolornyong@users.noreply.github.com>
2024-11-15 23:33:56 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 1423.31 ms, Memory: 34.9 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
@@ -1,26 +1,22 @@
 def solution(diffs, times, limit):
-    left, right = 1, max(diffs)
-    answer = 0
+    result = 0
+    left = 1
+    right = max(diffs)
     
     while left <= right:
-        need_time = 0
         mid = (left+right)//2
+        answer = 0
         
         for i in range(len(diffs)):
-            if diffs[i]<= mid:
-                need_time += times[i]
+            if diffs[i]<=mid:
+                answer += times[i]
             else:
-                need_time += (times[i] + times[i-1])*(diffs[i]-mid) + times[i]
-                
-            if need_time > limit:
-                break
-            
-        if need_time <= limit:
-            answer = mid
+                answer += (times[i-1]+times[i])*(diffs[i]-mid)+times[i]
+
+        if answer <= limit:
+            result = mid
             right = mid - 1
-            
         else:
             left = mid + 1
-            
-    
-    return answer
+        
+    return result
